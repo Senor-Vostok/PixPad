47e8fd849c31c6be4b2e37344026e0b30bce6c6b
Albert <albert09.ufa@gmail.com>
2024-12-03 17:37:25 +0500

    Добавление отрисовки кисти по векторному изображению

--- obb/brush.py
+++ obb/brush.py
@@ -6,12 +6,51 @@ from PIL import Image
 
 class Brush:
     def __init__(self, pattern_path, color=(0, 128, 255, 255), size_coef=1):
+        self.pattern_path = 'C:/Users/alber/PixPad/data/brushes/test_brush/Vector.svg'  # Пока так
         self.ico = QPixmap(QImage(pattern_path))
+        self.send_pack = list()
+        self.cx = 0.00000
+        self.cy = 0.00000
+        self.rx = 0.00000
+        self.ry = 0.00000
+        self.geometry = [[[0, 0]],  # 1
+                         [[0, 0], [1, 0], [0, 1], [1, 1]],  # 2
+                         [[0, 0], [1, 0], [0, 1], [-1, 0], [0, -1]],  # 3
+                         ]
         self.color = color
+
         self.size = size_coef
 
+    def get_parametrs(self):
+        with open(self.pattern_path, mode='rt') as f:
+            for i in f.readlines():
+                if 'cx' in i and "inkscape" not in i:
+                    i = i.replace('cx="', '')
+                    i = i.replace('"', '')
+                    i = i.split()
+                    print(i)
+                    self.cx = float(i[0])
+                if 'cy' in i and "inkscape" not in i:
+                    i = i.replace('cy="', '')
+                    i = i.replace('"', '')
+                    i = i.split()
+                    print(i)
+                    self.cy = float(i[0])
+                if 'rx' in i and "inkscape" not in i:
+                    i = i.replace('rx="', '')
+                    i = i.replace('"', '')
+                    i = i.split()
+                    print(i)
+                    self.rx = float(i[0])
+                if 'ry' in i and "inkscape" not in i:
+                    i = i.replace('ry="', '')
+                    i = i.replace('"', '')
+                    i = i.split()
+                    print(i)
+                    self.ry = float(i[0])
+
     def resize(self, new_size):  # прописать логику увеличения кисти
-        self.size = new_size
+        pass
 
     def recolor(self, new_color):  # прописать логику изменения цвета
         self.color = new_color
@@ -26,11 +65,18 @@ class Brush:
         return self.ico
 
     def display_brush(self, canvas, xoy, k):
+        self.get_parametrs()
+        self.send_pack.clear()
         image_data = list(canvas.brush_frame.image.getdata())
         image_width, image_height = canvas.brush_frame.image.size
         for index in range(len(image_data)):
             x = index % image_width
             y = index // image_width
-            if (xoy.x() // k, xoy.y() // k) == (x, y):
-                image_data[index] = self.color
+            cx = xoy.x() // k
+            cy = xoy.y() // k
+            if (cx, cy) == (x, y) or (cx - self.rx < x < cx + self.rx and cy - self.ry < y < cy + self.ry):
+                image_data[index] = self.color # пока для отображения оставил
+                # вот передача пикселей self.send_pack.append(index)
         canvas.brush_frame.image.putdata(image_data)
+        return self.send_pack
+
